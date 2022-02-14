c486b56cec5c87cafa3360af468cf6594c2c89a6
David Tang <david.tang@epfl.ch>
2022-02-14 11:38:00 +0100

    feat: vscode debug

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  configureWebpack: {
+    devtool: "source-map",
+  },
   transpileDependencies: ["vuetify"],
 };
--- vetur.config.js
+++ vetur.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    projects: [
-        './frontend'
-    ]
-}
+  projects: ["./frontend"],
+};
