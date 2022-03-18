ae48af402fa4363b7e17980ea5e3402db79b1e6a
David Tang <david.tang@epfl.ch>
2022-03-18 09:47:42 +0100

    feat: appList icon

--- frontend/src/components/AppList.vue
+++ frontend/src/components/AppList.vue
@@ -23,13 +23,16 @@
                 <v-list-item-title class="text-h5 mb-1">
                   {{ app.title }}
                 </v-list-item-title>
-                <v-list-item-subtitle
-                  >{{ app.description }}
+                <v-list-item-subtitle>
+                  {{ app.description }}
                 </v-list-item-subtitle>
               </v-list-item-content>
 
               <v-list-item-avatar tile size="80">
-                <v-img :src="app.logo_img"></v-img>
+                <v-img v-if="app.logoImg" :src="app.logoImg"></v-img>
+                <v-icon v-if="app.logoIcon" color="black" x-large>
+                  {{ app.logoIcon }}
+                </v-icon>
               </v-list-item-avatar>
             </v-list-item>
 
@@ -46,30 +49,37 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Component, Vue } from "vue-property-decorator";
 
-export default Vue.extend({
-  name: "AppList",
-  data() {
-    return {
-      apps: [
-        {
-          title: "Green House Gaz",
-          to: "GreenHouseGaz",
-          logo_img: "/app_logo/ghg.png",
-        },
-        {
-          title: "Shelter Sustainability",
-          to: "ShelterSustainability",
-          logo_img: "/app_logo/shelter.png",
-          description:
-            "A tool supporting comparative assessments of environmental impacts, technical performance, habitability and affordability of shelter designs used in humanitarian relief operations. ",
-        },
-        { title: "Energy", to: "Energy", logo_img: "/app_logo/energy.png" },
-      ],
-    };
-  },
-});
+@Component
+export default class AppList extends Vue {
+  readonly apps: {
+    title: string;
+    to: string;
+    logoImg?: string;
+    logoIcon?: string;
+    description?: string;
+  }[] = [
+    {
+      title: "Green House Gaz",
+      to: "GreenHouseGaz",
+      logoImg: "/app_logo/ghg.png",
+    },
+    {
+      title: "Shelter Sustainability",
+      to: "ShelterSustainability",
+      logoImg: "/app_logo/shelter.png",
+
+      description:
+        "A tool supporting comparative assessments of environmental impacts, technical performance, habitability and affordability of shelter designs used in humanitarian relief operations. ",
+    },
+    {
+      title: "Energy",
+      to: "Energy",
+      logoIcon: "mdi-flash",
+    },
+  ];
+}
 </script>
 
 <style lang="scss" scoped>
