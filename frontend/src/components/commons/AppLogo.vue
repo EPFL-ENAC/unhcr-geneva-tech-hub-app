0aaa124a61e1169e9a231fd7af76876b838c5b5f
Pierre Guilbert <pierre.guilbert@epfl.ch>
2022-06-13 14:19:56 +0200

    chore(optimization): transform jpeg to webp and add label-aria

--- frontend/src/components/commons/AppLogo.vue
+++ frontend/src/components/commons/AppLogo.vue
@@ -7,7 +7,7 @@
       max-width="170"
       min-width="150"
     >
-      <a :href="logo.urlDescription" target="_blank">
+      <a :href="logo.urlDescription" target="_blank" :aria-label="logo.label">
         <v-img
           :height="logo.height || '100px'"
           contain
@@ -27,45 +27,54 @@ export default class AppLogo extends Vue {
   logos = [
     {
       imgPath: "/app_logo_new/geneva_tech_hub.png",
+      label: "geneva_tech_hub",
       urlDescription:
         "https://www.unhcr.org/publications/brochures/6229f4184/geneva-technical-hub-brochure.html",
     },
     {
       imgPath: "/app_logo_new/Logo_UNHCR.png",
+      label: "Logo_UNHCR",
       urlDescription:
         "https://www.unhcr.org/publications/brochures/6229f4184/geneva-technical-hub-brochure.html",
     },
     {
       imgPath: "/app_logo_new/sdc_2.png",
+      label: "geneva technical hub brochure",
       urlDescription:
         "https://www.unhcr.org/publications/brochures/6229f4184/geneva-technical-hub-brochure.html",
     },
 
     {
       imgPath: "/app_logo_new/EPFL.png",
+      label: "EPFL",
       urlDescription: "https://epfl.ch",
       height: "30px",
     },
     {
       imgPath: "/app_logo_new/EssentialTech_Logo_5.png",
+      label: "essential tech",
       urlDescription: "https://essentialtech.center/hth/",
     },
     {
       imgPath: "/app_logo_new/gnasounougroup1.jpg",
+      label: "energy planning lab",
       urlDescription: "https://www.epfl.ch/labs/bpe/",
     },
     {
       imgPath: "/app_logo_new/SLL-LOGO-SQUARE-RVB.png",
+      label: "smart living lab",
       urlDescription: "https://www.smartlivinglab.ch/fr/",
       height: "60px",
     },
     {
-      imgPath: "/app_logo_new/Logo_FAR.jpg",
+      imgPath: "/app_logo_new/Logo_FAR.webp",
+      label: "FAR",
       urlDescription: "https://www.epfl.ch/labs/far/",
       height: "60px",
     },
     {
       imgPath: "/app_logo_new/enac-it4r.png",
+      label: "enac-it4r",
       urlDescription: "https://enac-it4r.epfl.ch/",
     },
   ];
