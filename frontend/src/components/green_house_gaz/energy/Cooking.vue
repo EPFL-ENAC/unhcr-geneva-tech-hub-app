1152a28bf47a52de9a8c1942b897925a7bac7192
Pierre Guilbert <pierre.guilbert@epfl.ch>
2023-01-06 16:31:01 +0100

    feat(energy-cooking): formula for co2

--- frontend/src/components/green_house_gaz/energy/Cooking.vue
+++ frontend/src/components/green_house_gaz/energy/Cooking.vue
@@ -1,13 +1,13 @@
 <template>
   <v-container fluid>
     <survey-item-title :title-key="title" />
-    <!-- <baseline-endline-wrapper
+    <baseline-endline-wrapper
       v-model="localForm"
       :headers="headers"
       :diff-dimension="diffDimension"
       :compute-item="computeItem"
       :name="name"
-    /> -->
+    />
   </v-container>
 </template>
 
@@ -21,27 +21,170 @@ import {
   SurveyItem,
   SurveyResult,
 } from "@/store/GhgInterface.vue";
+import { get as _get } from "lodash";
 
+import { ItemReferencesMap } from "@/store/GhgReferenceModule";
 import "vue-class-component/hooks";
 import { Component, Prop, Vue } from "vue-property-decorator";
 
-// const Wood = "Wood";
-// const Charcoal = "Charcoal";
-// const Pellets = "Pellets";
-// const Ethanol = "Ethanol";
-// const Kerosene = "Kerosene";
-// const Electric = "Electric";
-// const Thermal = "Thermal solar";
-// const LPG = "LPG";
-// const BIOGAS = "BIOGAS";
-// const UNKNOWN = "UNKNOWN";
+const fuelTypes = {
+  FWD: "Wood",
+  CHC: "Charcoal",
+  PLTS: "Pellets",
+  BRQ: "Briquette",
+  ETH: "ETHANOL",
+  KRS: "KEROSENE",
+  ELE: "Electric", // no eff
+  THE: "Thermal solar", // no eff
+  CNDL: "candle", // no use
+  LPG: "LPG",
+  BGS: "BIOGASS",
+  UNK: "UNKNOWN",
+};
+// TODO: findout if it's ever used ?
+const cookstoveTypes = {
+  UNK: "UNKNOWN",
+  CONV: "CONVENTIONAL",
+  IMPR: "IMPROVED",
+  TRAD: "Traditional",
+};
 
+// no appliances for now
 // const COOK_APP_Pressure = "Pressure cooke";
 // const COOK_APP_Heat = "Heat retaining basket";
 // const COOK_APP_Other = "Other";
 
-const COOK_APP_CONV = "CONVENTIONAL";
-const COOK_APP_CLEAN = "Clean";
+const cookstoveTECHs = [
+  {
+    _id: "1",
+    text: "Without any access (no possibility to cook)",
+    image: undefined,
+    fuelType: "UNK",
+    type: "UNK",
+  },
+  {
+    _id: "2",
+    text: "Using firewood traditional (three stone)",
+    image: "/images/energy/cookstoves/traditional-wood.png",
+    fuelType: "FWD",
+    type: "TRAD",
+  },
+  {
+    _id: "3",
+    text: "Using charcoal (traditional)",
+    image: undefined,
+    fuelType: "CHC",
+    type: "TRAD",
+  },
+  {
+    _id: "4",
+    text: "Using firewood improved cookstove",
+    image: undefined,
+    fuelType: "FWD",
+    type: "IMPR",
+  },
+  {
+    _id: "5",
+    text: "Using charcoal improved cookstove",
+    image: undefined,
+    fuelType: "CHC",
+    type: "IMPR",
+  },
+  {
+    _id: "6",
+    text: "Using briquettes and improved cookstove",
+    image: undefined,
+    fuelType: "BRQ",
+    type: "IMPR",
+  },
+  {
+    _id: "7",
+    text: "Using pelets and improved cookstove",
+    image: "/images/energy/cookstoves/gasifier.png",
+    fuelType: "PLTS",
+    type: "IMPR",
+  },
+  {
+    _id: "8",
+    text: "Using firewood with gasifier",
+    image: undefined,
+    fuelType: "FWD",
+    type: "IMPR",
+  },
+  {
+    _id: "9",
+    text: "Using charcoal with gasifier",
+    image: undefined,
+    fuelType: "CHC",
+    type: "IMPR",
+  },
+  {
+    _id: "10",
+    text: "Using briquette with gasifier",
+    image: undefined,
+    fuelType: "BRQ",
+    type: "IMPR",
+  },
+  {
+    _id: "11",
+    text: "Using ethanol",
+    image: "/images/energy/cookstoves/ethanol.png",
+    fuelType: "ETH",
+    type: "CONV",
+  },
+  {
+    _id: "12",
+    text: "Using LPG",
+    image: "/images/energy/cookstoves/biogas.png",
+    fuelType: "LPG",
+    type: "CONV",
+  },
+  {
+    _id: "13",
+    text: "Using biogas",
+    image: undefined,
+    fuelType: "BGS",
+    type: "CONV",
+  },
+  {
+    _id: "14",
+    text: "Using kerosene",
+    image: undefined,
+    fuelType: "KRS",
+    type: "CONV",
+  },
+  {
+    _id: "15",
+    text: "Connected to the national grid",
+    image: undefined,
+    fuelType: "ELE",
+    type: "CONV",
+  },
+  {
+    _id: "16",
+    text: "Using electricity from diesel generators",
+    image: undefined,
+    fuelType: "ELE", // TODO fixme with new fuel type ?
+    type: "CONV",
+  },
+  {
+    _id: "17",
+    text: "Using electricity from 100% renewable minigrid (solar PV + batteries)",
+    image: "/images/energy/cookstoves/solar-box.png",
+    fuelType: "ELE", // TODO fixme with new fuel type efficiency ?
+    type: "CONV",
+  },
+  {
+    _id: "18",
+    text: "Using electricity from hybrid micro-grid (DG+Solar PV+ grid)",
+    image: undefined,
+    fuelType: "ELE",
+    type: "CONV",
+  },
+];
+const APPLIANCE_EFFICIENCY = 1;
+const REF_DRY_WOOD = 0.85; // weird; we're missing information
+const fNRB = 1;
 
 @Component({
   components: {
@@ -56,7 +199,7 @@ export default class Trucking extends Vue {
   @Prop({ type: [Object, Array] })
   readonly form!: EnergyCookingSurvey;
 
-  diffDimension: keyof EnergyCookingItemInput = "cardinal";
+  diffDimension: keyof EnergyCookingItemInput = "numberOfCookstove";
   name = "cookstove";
 
   public get title(): string {
@@ -71,10 +214,34 @@ export default class Trucking extends Vue {
   }
 
   public computeItem(
-    localItemInput: EnergyCookingItemInput
+    localItemInput: EnergyCookingItemInput,
+    ghgMapRef: ItemReferencesMap
   ): EnergyCookingItemResults {
+    const { numberOfCookstove, fuelUsage, fuelType } = localItemInput;
+    /*
+     **  CO2 Emissions =
+     **      Number_of_Cookstove
+     **    * FUEL_USAGE [kg or L/day]
+     **    * 365,25 [Days/Yr]
+     **    * REF_EF_{FUEL} // TODO have a matching table between fuel type and REF_EFF
+     **    * TIME_EFFICIENCY
+     **    * fNRB
+     **    * REF_DRY_WOOD
+     **    * (IF CHARCOAL=YES : Wood_to_charcoal factor a.k.a 6)
+     **    / 1000 [kg/t]
+     */
+    const totalCO2Emission =
+      (numberOfCookstove * // NEED FORMULA
+        fuelUsage * // use selection
+        365.25 *
+        ghgMapRef?.[`REF_EFF_${fuelType}`]?.value ??
+        1 *
+          APPLIANCE_EFFICIENCY * // should be 1 for now (1 - 0.7 or 0.3 depending on appliances)
+          fNRB * // country or default value of 0.76 or 0.85 who knows or 1 if not in list of fuel
+          REF_DRY_WOOD * // should be 1 for now
+          (fuelType === "CHC" ? 6 : 1)) / 1000;
     return {
-      totalCO2Emission: localItemInput.cardinal * 30,
+      totalCO2Emission,
     };
   }
 
@@ -96,49 +263,78 @@ export default class Trucking extends Vue {
       value: "increment",
       type: "number",
       hideFooterContent: false,
-      formatter: (value: number) => {
-        // https://stackoverflow.com/a/30686832
-        function n2s(n: number) {
-          let s = "";
-          if (!n) s = "a";
-          else
-            while (n) {
-              s = String.fromCharCode(97 + (n % 26)) + s;
-              n = Math.floor(n / 26);
-            }
-          return s;
-        }
-        return n2s(value);
+      baselineOnly: true,
+      formatter: this.n2sFormatter,
+    },
+    {
+      text: "#", // unique name === dropdown of existant facilities
+      value: "originIncrement",
+      endlineOnly: true,
+      type: "number",
+      hideFooterContent: false,
+      formatter: (
+        v: number,
+        _: unknown,
+        item: SurveyItem,
+        items: SurveyItem[]
+      ) => {
+        const increment: number = _get(item, "increment");
+        // todo finish custom increment function
+        const increments = items
+          .filter((item: SurveyItem) => item.originIncrement === v)
+          .map((item: SurveyItem) => item.increment);
+        const indexOf = increments.indexOf(increment);
+        return `${this.n2sFormatter(v)}${"'".repeat(indexOf)}`;
+      },
+      formatterOrigin: (v: number) => {
+        return `${this.n2sFormatter(v)}`;
       },
     },
     {
-      text: "Intervention",
-      value: "input.description",
-      type: "text",
-
-      endlineOnly: true,
+      text: "Cookstove",
+      value: "input.cookstove",
+      type: "select",
+      style: {
+        cols: "12",
+      },
       hideFooterContent: false,
+      items: cookstoveTECHs,
+      customEventInput: (cookstoveId: string, localInput: SurveyInput) => {
+        console.log(cookstoveId, localInput);
+        // does not work with reference ?
+        localInput.fuelType = "CHC";
+        return localInput;
+      },
     },
     {
+      text: "Fuel per day (kg/day for biomass)",
+      value: "input.fuelUsage",
+      conditional_value: "",
+      conditional: "cookstove",
+      style: {
+        cols: "12",
+      },
+      type: "number",
+    },
+
+    {
       text: "Number of cookstove",
-      value: "input.TR_VOL",
-      conditional_value: "KM",
-      conditional: "US_UNI",
+      computeResults: true,
+      value: "input.numberOfCookstove",
+      conditional_value: "",
+      conditional: "fuelUsage",
       style: {
         cols: "12",
       },
       type: "number",
     },
     {
-      text: "Cookstove techno",
-      value: "input.cookTechno",
-      type: "select",
-      hideFooterContent: false,
-      items: [COOK_APP_CONV, COOK_APP_CLEAN],
-    },
-    {
       text: "Cookstove fuel",
       value: "input.fuelType",
+      formatter: (v: keyof typeof fuelTypes) => {
+        return fuelTypes[v];
+      },
+      isInput: false,
       type: "select",
       hideFooterContent: false,
       items: [],
@@ -150,6 +346,7 @@ export default class Trucking extends Vue {
       formatter: (v: number, { ...args }) => {
         return formatNumber(v, { suffix: args.suffix });
       },
+      computeResults: true,
       type: "number",
       disabled: true,
     },
@@ -181,7 +378,7 @@ export default class Trucking extends Vue {
     },
   ].map((item) => {
     const [category, key] = item.value.split(".");
-    const isInput = category === "input";
+    const isInput = item?.isInput ?? category === "input";
     return {
       align: "start",
       sortable: false,
@@ -193,16 +390,25 @@ export default class Trucking extends Vue {
       formatter: (value: unknown) => value,
       classFormatter: () => "",
       options:
-        item?.items?.map((item) => ({ text: item, value: item })) ?? undefined,
+        item?.items?.map((item) => {
+          if (typeof item === "string") {
+            return { text: item, value: item };
+          }
+          return {
+            text: item?.text,
+            value: item?._id,
+          };
+        }) ?? undefined,
       ...item,
     };
   });
 }
 
 export interface EnergyCookingItemInput extends SurveyInput {
+  numberOfCookstove: number; // computed based on % of HH and stuffs
+  fuelUsage: number; // [kg or L/day]
   cookTechno: string;
-  fuelType: string;
-  cardinal: number;
+  fuelType: string; // key
 }
 
 export interface EnergyCookingItemResults extends SurveyResult {
