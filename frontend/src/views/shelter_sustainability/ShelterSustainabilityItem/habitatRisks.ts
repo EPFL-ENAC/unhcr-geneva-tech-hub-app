fdbaff5889bafc91031f8ff42af31684b5fced23
Pierre Guilbert <pierre.guilbert@epfl.ch>
2023-02-20 11:40:24 +0100

    fix(shelter): better text for habitat and reuse fixing issue #132

--- frontend/src/views/shelter_sustainability/ShelterSustainabilityItem/habitatRisks.ts
+++ frontend/src/views/shelter_sustainability/ShelterSustainabilityItem/habitatRisks.ts
@@ -9,12 +9,12 @@ export default [
   {
     id: "Aluminium",
     description:
-      "Aluminium production involves relatively larges amounts of mineral bauxite mining/extraction, which is associated with larges amounts of unused residues (<a href='https://www.greenspec.co.uk/building-design/aluminium-production-environmental-impact/' target='_blank'>Greenspec, 2022</a>). Primary mining areas for bauxite are Australia, China, Brazil and India. Primary production of aluminium is highly energy intensive - requiring around 211 GJ per tonne of aluminium, compared to 22.7GJ per tonne for steel (<a href='https://publications.csiro.au/rpr/download?pid=csiro:EP12183&dsid=DS3' target='_blank'>CSIRO, 2021</a>). However, around a third of all aluminium production globally comes from reuse of scrap from manufacturing and from recycling (<a href='https://www.iea.org/reports/aluminium' target='_blank'>IEA, 2022</a>).",
+      "Aluminium production involves relatively larges amounts of mineral bauxite mining/extraction, which is associated with larges amounts of unused residues. Primary mining areas for bauxite are Australia, China, Brazil and India. Primary production of aluminium is highly energy intensive - requiring around 211 GJ per tonne of aluminium, compared to 22.7GJ per tonne for steel, depending upon specific production processes. However, around a third of all aluminium production globally comes from reuse of scrap from manufacturing and from recycling (IEA, 2022, Aluminium <www.iea.org/reports/aluminium>).",
   },
   {
     id: "Bamboo",
     description:
-      "Bamboo is widely associated with less harmful environmental impacts in comparison with other structural framing materials such as timber or steel. However, total quantities of bamboo use should be taken into account with regard to numbers of shelters to be provided. Poorly managed bamboo extraction can contribute to erosion (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>). Bamboo has relatively short harvest cycles of around 4-6 years. Bamboo growth is associated with extensive irrigation, however bamboo plantations typically rely on rainfall in tropical climates.",
+      "Bamboo is widely associated with less harmful environmental impacts in comparison with other structural framing materials such as timber or steel. However, total quantities of bamboo use should be taken into account with regard to numbers of shelters to be provided. Poorly managed bamboo extraction can contribute to erosion (WWF, 2016. Building Material Selection - An environmental guide). Bamboo has relatively short harvest cycles of around 4-6 years. Bamboo growth is associated with extensive irrigation, however bamboo plantations typically rely on rainfall in tropical climates.",
   },
   {
     id: "Bitumen",
@@ -24,32 +24,32 @@ export default [
   {
     id: "Canvas, Polycotton",
     description:
-      "Canvas and polycotton production derives from cotton cultivation, which is associated with high water use for irrigation and extensive use of pesticides (US Dept of Agriculture, 2020 [U.S. Department of Agriculture (2020a) 2019 Agricultural chemical use survey: cotton. NASS Highlights. No. 2020-3.]. Polycotton contains polyester, which uses less water in production compared to natural fibres, though invovles relatively energy intensive production - around 125MJ per kg (Muthu, S.S., 2020. Assessing the environmental impact of textiles and the clothing supply chain. Woodhead publishing.)",
+      "Canvas and polycotton production derives from cotton cultivation, which is associated with high water use for irrigation and extensive use of pesticides. Polycotton contains polyester, which uses less water in production compared to natural fibres, though invovles relatively energy intensive production - around 125MJ per kg (Muthu, 2020. Assessing the environmental impact of textiles and the clothing supply chain)",
   },
   {
     id: "Cement",
     description:
-      "Cement production involves extensive limestone and shale quarrying. Cement production is typically concentrated near limestone sources. More than half of global cement production occurs in China ((<a href='https://www.iea.org/reports/cement' target='_blank'>IEA, 2022</a>). Clinker production - a principal component of cement production - is energy intensive, involving around 3.5GJ of energy per tonne of clinker ((<a href='https://www.iea.org/reports/cement' target='_blank'>IEA, 2022</a>).",
+      "Cement production involves extensive limestone and shale quarrying. Cement production is typically concentrated near limestone sources. More than half of global cement production occurs in China (IEA, 2022. Cement, <www.iea.org/reports/cement>). Clinker production - a principal component of cement production - is energy intensive, involving around 3.5GJ of energy per tonne of clinker (IEA, 2022 <https://www.iea.org/reports/cement>).",
   },
   {
     id: "Clay",
     description:
-      "Clay is a specific type of fine-grained soil. Sourcing of clay in large quantities from suitable sites can contribute to erosion and alter local hydrological conditions (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>). Fired clay brick and tile production involves baking filns that may use wood or waste such as discarded car tires that contributes to deforestation and air pollution. In some countries (inc., Afghanistan, Burma, China, India, Nepal and Pakistan), clay brick production is associated with indentured labour (<a href='https://www.dol.gov/agencies/ilab/reports/child-labor/list-of-products-print' target='_blank'>US Dept of Labour, 2022</a>)",
+      "Clay is a specific type of fine-grained soil. Sourcing of clay in large quantities from suitable sites can contribute to erosion and alter local hydrological conditions (WWF, 2016. Building Material Selection - An environmental guide). Fired clay brick and tile production involves baking filns that may use wood or waste such as discarded car tires that contributes to deforestation and air pollution. In some countries (inc., Afghanistan, Burma, China, India, Nepal and Pakistan), clay brick production is associated with indentured labour (US Dept of Labour, 2022 <https://www.dol.gov/agencies/ilab/reports/child-labor/list-of-products-print>)",
   },
   {
     id: "Concrete",
     description:
-      "Concrete production typically involves cement, sand, stone aggregate and water. Cement production involves production of clinker, which is energy intensive, involving around 3.5GJ of energy per tonne of clinker ((<a href='https://www.iea.org/reports/cement' target='_blank'>IEA, 2022</a>). Poorly managed quarrying of sand and stone aggregates can contribute to erosion and loss of agricultural land (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>). Movement of bulk materials for concrete entails transportation-related envrionmental impacts. Concrete production, including mixing and curing, involves significant water use. Casting yards for concrete blocks and other precast concrete can cause daust, noise and silt problems (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>).",
+      "Concrete production typically involves cement, sand, stone aggregate and water. Cement production involves production of clinker, which is energy intensive, involving around 3.5GJ of energy per tonne of clinker (IEA, 2022 <www.iea.org/reports/cement>). Poorly managed quarrying of sand and stone aggregates can contribute to erosion and loss of agricultural land (WWF, 2016. Building Material Selection - An environmental guide). Movement of bulk materials for concrete entails transportation-related envrionmental impacts. Concrete production, including mixing and curing, involves significant water use. Casting yards for concrete blocks and other precast concrete can cause daust, noise and silt problems (WWF, 2016. Building Material Selection - An environmental guide).",
   },
   {
     id: "Earth, soil, mud",
     description:
-      "Soil for construction purposes includes clay and other classes of soil. Soil quarrying in general can contribute to erosion and loss of agricultural land (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>). Clay is a specific type of fine-grained soil. Sourcing of clay in large quantities from suitable sites can contribute to erosion and alter local hydrological conditions (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>). Fired clay brick and tile production involves baking filns that may use wood or waste such as discarded car tires that contributes to deforestation and air pollution. In some countries (inc., Afghanistan, Burma, China, India, Nepal and Pakistan), clay brick production is associated with indentured labour (<a href='https://www.dol.gov/agencies/ilab/reports/child-labor/list-of-products-print' target='_blank'>US Dept of Labour, 2022</a>)",
+      "Soil for construction purposes includes clay and other classes of soil. Soil quarrying in general can contribute to erosion and loss of agricultural land (WWF, 2016. Building Material Selection - An environmental guide). Clay is a specific type of fine-grained soil. Sourcing of clay in large quantities from suitable sites can contribute to erosion and alter local hydrological conditions (WWF, 2016. Building Material Selection - An environmental guide). Fired clay brick and tile production involves baking filns that may use wood or waste such as discarded car tires that contributes to deforestation and air pollution. In some countries (inc., Afghanistan, Burma, China, India, Nepal and Pakistan), clay brick production is associated with indentured labour (US Dept of Labour, 2022 <www.dol.gov/agencies/ilab/reports/child-labor/list-of-products-print>)",
   },
   {
     id: "Glass",
     description:
-      "Glass production is energy intensive (reflected in embodied energy of glass around 15.9 MJ/kg (Atkins, C., 2009. Sustainability of glass in construction. Sustainability of Construction Materials, Woodhead Publishing)",
+      "Glass production is energy intensive, reflected in embodied energy of glass around 15.9 MJ/kg (Atkins, 2009. Sustainability of glass in construction. Sustainability of Construction Materials)",
   },
   {
     id: "Plastic, polymer",
@@ -59,21 +59,21 @@ export default [
   {
     id: "Sand",
     description:
-      "Sand sourced through poorly managed quarrying can contribute to erosion and loss of agricultural land (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>)",
+      "Sand sourced through poorly managed quarrying can contribute to erosion and loss of agricultural land (WWF, 2016. Building Material Selection - An environmental guide)",
   },
   {
     id: "Steel",
     description:
-      "Steel production is energy intensive, with energy in production of around 18GJ/tonne (<a href='https://www.greenspec.co.uk/building-design/steel-products-and-environmental-impact/' target='_blank'>GeenSpec, 2022</a>). However, steel is readily recycled - scrap-based production, which is considerably less energy intensive, accounts for around 20% of global steel production (<a href='https://www.iea.org/reports/iron-and-steel' target='_blank'>IEA, 2022</a>).",
+      "Steel production is energy intensive, with energy in production of around 18GJ/tonne. However, steel is readily recycled - scrap-based production, which is considerably less energy intensive, accounts for around 20% of global steel production (IEA, 2022 <www.iea.org/reports/iron-and-steel>).",
   },
   {
     id: "Stone",
     description:
-      "Stone quarrying, including stone aggregates, can contribute to erosion and loss of agricultural land (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>). Movement of bulk materials such as stone and aggregates entails transportation-related envrionmental impacts.",
+      "Stone quarrying, including stone aggregates, can contribute to erosion and loss of agricultural land (WWF, 2016. Building Material Selection - An environmental guide). Movement of bulk materials such as stone and aggregates entails transportation-related envrionmental impacts.",
   },
   {
     id: "Timber",
     description:
-      "Timber use in construction can contribute to deforestation and associated erosion and loss of natural habitats. Poorly managed timber plantations can cause land degradation and loss of biodiversity (<a href='https://www.worldwildlife.org/publications/an-environmental-guide-to-building-material-selection-and-use' target='_blank'>WWF, 2016 [Building Material Selection - An environmental guide]</a>).",
+      "Timber use in construction can contribute to deforestation and associated erosion and loss of natural habitats. Poorly managed timber plantations can cause land degradation and loss of biodiversity (WWF, 2016. Building Material Selection - An environmental guide).",
   },
 ];
