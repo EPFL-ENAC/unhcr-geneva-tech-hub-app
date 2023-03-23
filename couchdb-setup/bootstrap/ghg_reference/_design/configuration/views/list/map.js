b811b43a6cce4311798dd5cf4fad7b4e64f2ee34
Pierre Guilbert <pierre.guilbert@epfl.ch>
2023-03-23 05:23:16 +0100

    chore(database): remove unused reference database

--- couchdb-setup/bootstrap/ghg_reference/_design/configuration/views/list/map.js
+++
@@ -1,4 +0,0 @@
-function (doc) {
-    // retrieve only fields necessary for listing shelters
-      emit(doc._id, {description: doc.description, value: doc.value, type: doc.type, source: doc.source, _id: doc._id});
-}
--- couchdb-setup/bootstrap/ghg_reference_iges_grid/_design/countries/views/list/map.js
+++
@@ -1,4 +0,0 @@
-function (doc) {
-    // retrieve only fields necessary for listing shelters
-      emit(doc._id, {name: doc.name, value: doc.value, _id: doc._id});
-}
